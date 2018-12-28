d10cc4f0552201f89e031df8a4a854e045fe546b
Rejectt <45071435+Rejectt@users.noreply.github.com>
2018-12-28 12:10:38 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -60,7 +60,7 @@ client.on('ready', () => {
 });
 client.on('ready', function(){
     var ms = 100000 ;
-    var setGame = [`!help Kings Net Work `];
+    var setGame = [`!help Black Bloods Net Work `];
     var i = -1;
     var j = 0;
     setInterval(function (){
